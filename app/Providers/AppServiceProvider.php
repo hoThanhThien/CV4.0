e73c4d90c57c49c64a7f8cc535a32d5d66dbfd54
hoThanhThien <thienht2941@ut.edu.vn>
2026-09-20 23:22:38 +0700

    fix: trust proxy and force HTTPS scheme on Render to prevent mixed content form warning

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.env') === 'production' || env('APP_ENV') === 'production' || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
